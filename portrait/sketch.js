a5349614abb6a55fc6c5415fe021e2e6840d97b2
Sam <ss11976@nyu.edu>
2022-02-09 00:59:27 -0500

    self-portrait

---
+++ portrait/sketch.js
@@ -0,0 +1,20 @@
+$(".b1").click(function() {
+    $(this).text("this is sam! she is very friendly.");
+    $(".b1").css("color", "rgb(236, 183, 241)");
+})
+$(".b2").click(function() {
+    $(this).text("sam is her fourth year at nyu and is studying digital media.");
+    $(".b2").css("color", "rgb(236, 183, 241)");
+})
+$(".b3").click(function() {
+    $(this).text("sam is twenty-one years old and she is a leo.");
+    $(".b3").css("color", "rgb(236, 183, 241)");
+})
+$(".b4").click(function() {
+    $(this).text("sam enjoys editing videos and storytelling.");
+    $(".b4").css("color", "rgb(236, 183, 241)");
+})
+$(".b5").click(function() {
+    $(this).text("sam is from bangalore, india and uses she/her/hers pronouns.");
+    $(".b5").css("color", "rgb(236, 183, 241)");
+})
